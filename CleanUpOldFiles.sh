425c7fa85d6618de1de0a488027e97af2e90d577
AIHR <50837811+AIHRSec@users.noreply.github.com>
2024-04-02 21:16:33 -0400

    Update CleanUpOldFiles.sh

--- CleanUpOldFiles.sh
+++ CleanUpOldFiles.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# Variable of current year and month plus wildcard 
-month=$(date '+%Y-%m')-*
+# Variable of previous year and month plus wildcard
+previous_month=$(date --date='-1 month' '+%Y-%m')
 
 # Deletes all the files that follow the variable format YYYY-MM-*
 #rm $month
