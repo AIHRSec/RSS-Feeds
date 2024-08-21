21b2c6679f116f5e6fa7a46bcdaca47bdc8d5754
AIHR <50837811+AIHRSec@users.noreply.github.com>
2024-08-21 00:29:15 -0400

    Create run_temp_check.sh

---
+++ run_temp_check.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Set the environment variable
+export RSSVar=$HOME/ragno/rango-dev/bin/python3
+
+# Change directory to where the config file is located
+cd $HOME/ragno
+
+# Runs the Python script using the Python interpreter specified ENV
+$HOME/ragno/rango-dev/bin/python3 $HOME/ragno/demo/temp_checker.py
