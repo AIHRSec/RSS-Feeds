eaa315e69132f2f1bbdd95060bc2bff2b7ec4ffc
AIHR <50837811+AIHRSec@users.noreply.github.com>
2024-08-20 21:35:03 -0400

    Update CleanUpOldFiles.sh

--- CleanUpOldFiles.sh
+++ CleanUpOldFiles.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-# Variable of previous year and month plus wildcard
+# Variable of pervious year and month plus wildcard
 previous_month=$(date --date='-1 month' '+%Y-%m')-*
 
 # For testing only
-ls ~/output/$previous_month > ~/logs/Previous_Month_Files.txt
+ls $HOME/ragno/output/$previous_month > $HOME/ragno/logs/Previous_Months_Files.txt
 
 # Deletes all the files that follow the variable format YYYY-MM-*
-rm ~/output/$previous_month
+rm $HOME/ragno/output/$previous_month
