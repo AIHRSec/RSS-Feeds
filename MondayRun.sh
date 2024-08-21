c5bfeefb67a5dfe1de861b9181802aa74ae4c10b
AIHR <50837811+AIHRSec@users.noreply.github.com>
2024-08-20 21:32:40 -0400

    Create MondayRun.sh

---
+++ MondayRun.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Set the environment variable
+export RSSVar=$HOME/ragno/rango-dev/bin/python3
+
+# Change directory to where the config file is located
+cd $HOME/ragno
+
+# Runs the Python script using the Python interpreter specified ENV
+$HOME/ragno/rango-dev/bin/python3 $HOME/ragno/scripts/MondaySpecial.py
+
+# Moves output
+mv 2024-* ./output
