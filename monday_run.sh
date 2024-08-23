bd0064525d773b24bc86b3247bc4c11013551d6a
AIHR <50837811+AIHRSec@users.noreply.github.com>
2024-08-22 20:52:52 -0400

    Update and rename MondayRun.sh to monday_run.sh

--- monday_run.sh
+++ monday_run.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# This script is used to start the monday_special python script
+# monday_special pulls news over the last 70 hours
 
 # Set the environment variable
 export RSSVar=$HOME/ragno/rango-dev/bin/python3
@@ -7,7 +9,7 @@ export RSSVar=$HOME/ragno/rango-dev/bin/python3
 cd $HOME/ragno
 
 # Runs the Python script using the Python interpreter specified ENV
-$HOME/ragno/rango-dev/bin/python3 $HOME/ragno/scripts/MondaySpecial.py
+$HOME/ragno/rango-dev/bin/python3 $HOME/ragno/scripts/monday_special.py
 
 # Moves output
 mv 2024-* ./output
